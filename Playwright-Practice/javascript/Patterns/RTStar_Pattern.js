766d12ff213eefaab262f2884fe0bf3677391466
omgutty <om.gutty@gmail.com>
2026-07-07 12:30:00 +0530

    feat: Right angle pattern star

---
+++ Playwright-Practice/javascript/Patterns/RTStar_Pattern.js
@@ -0,0 +1,25 @@
+/**
+ * Print a right triangle pattern using stars. For input n, print n rows where row i has i stars.
+
+Input: 3
+Output: * ** ***
+ */
+
+const readline=require("readline");
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question("Input: ", (answer)=>{
+    const num=parseInt(answer);
+
+    for (let i = 1; i <= num; i++){
+        let pattern = "";
+        for(let j=1;j<=i;j++){
+              pattern += "*";
+        }
+        console.log(pattern);
+    }
+rl.close();
+});
---
+++ Playwright-Practice/javascript/Patterns/left_pattern.js
@@ -0,0 +1,18 @@
+/**
+ * Print a left-aligned triangle pattern. For input n, print n rows in reverse order.
+
+Input: 3
+Output: *** ** *
+ */
+
+const readline=require("readline");
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question("Input: ", (answer)=>{
+    const num=parseInt(answer);
+    
+
+});
