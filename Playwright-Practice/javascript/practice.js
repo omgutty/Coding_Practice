3a708d5eaff7b98afe662332e8230270853bf228
omgutty <om.gutty@gmail.com>
2026-09-07 16:22:00 +0530

    reverse

--- Playwright-Practice/javascript/practice.js
+++ Playwright-Practice/javascript/practice.js
@@ -2,3 +2,17 @@
 
 let arr= [12,3,4,5,6,7]
 
+//option 1
+let largest= Math.max(...arr)
+console.log(largest)
+
+//option 2
+let largest2 = arr[0]
+
+for (let i=1;i<arr.length;i++){
+    if(arr[i]>largest){
+        largest2=arr[i]
+    }
+}
+
+console.log(largest2)
